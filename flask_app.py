262210026336333b58036ae4ac42fa9e331744a9
bakerso1 <bakerso1@msu.edu>
2023-01-28 17:44:26 -0500

    send it

---
+++ flask_app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=81, debug=True)
---
+++ openai_request_script.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import json
+
+def send_request():
+    # Define the API endpoint and your API key
+    api_endpoint = "https://api.openai.com/v1/engines/text-davinci-002/completions"
+    api_key = os.environ["OPENAI_API_KEY"]
+
+    # Define the input parameters for the API request
+    data = {
+        "prompt": "What is 5+7?",
+        "temperature": 0.5,
+        "max_tokens": 128,
+    }
+
+    # Make the API request
+    response = requests.post(api_endpoint, json=data, headers={ "Authorization": f"Bearer {api_key}"})
+
+    # Print the response
+    data = json.dumps(response.json(), indent=4)
+
+    # Edit response for sending
+    text = ""
+    temp = response.json()
+    if "choices" in temp:
+        choices = temp["choices"][0]
+        if "text" in choices:
+            text = choices["text"]
+
+            if "finish_reason" in choices:
+                finish_reason = choices["finish_reason"]
+                if finish_reason == "length":
+                    text = text + "\n\nDue to testing response was cut short to cost less."
+                if finish_reason == "stop":
+                    pass # succsessful full response
+                else:
+                    print("REASON NOT ACCOUNTED FOR:", finish_reason)
+            
+    # response for user
+    return text
+
+
