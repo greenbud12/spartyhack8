6004b3a5913c9ce3220b9aeb6474f3b61add6ef0
bakerso1 <bakerso1@msu.edu>
2023-01-29 02:02:08 -0500

    Alpha

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,7 @@
 from flask import current_app as app
 from flask import Flask, request, redirect
 
-# from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse
 from . openai_request_script import send_request
 from . twilio_script import twii
 
@@ -25,32 +25,46 @@ def index():
 	        """
 	
 
-@app.route("/sms", methods=['GET', 'POST'])
+@app.route("/sms/call", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
-    # resp = MessagingResponse()
+    resp = MessagingResponse()
 
     # Add a message
-    # resp.message("We plan to support voice to text in the future!")
+    resp.message("The Robots are coming! Head for the hills!")
 
-    return "HIHIHIHIHIHIHI"#str(resp)
+    return str(resp)
 
-@app.route("/sms", methods=['GET', 'POST'])
+@app.route("/sms/msg", methods=['GET', 'POST'])
 def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # MSG to send back
+    msg = "A test"
+
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
+    b2 = body.lower().strip()
 
-    # Start our TwiML response
-    # resp = MessagingResponse()
-    twi = twii()
+    # # Determine the right reply for this message
+    if b2 == 'hello':
+        resp.message("Hi!")
+    elif b2 == 'bye':
+        resp.message("Goodbye")
+    elif b2 == 'help' or b2 == 'h':
+        resp.message("I am a chatbot that can answer any of your questions.\
+             You could ask about the weather, color of an apple, or how to plant\
+                a tomato seedling. Your creativity is the limit.")
+    else:
+        # Add a message
+        msg = resp.message(send_request(body))
 
-    # Determine the right reply for this message
-    # if body == 'hello':
-    #     resp.message("Hi!")
-    # elif body == 'bye':
-    #     resp.message("Goodbye")
-    twi.send_msg("sup dude")
+        # Add a picture message
+        msg.media(
+            "https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg"
+        )
 
-    return str(twi)
+    return str(resp)
